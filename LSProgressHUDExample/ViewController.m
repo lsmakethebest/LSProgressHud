b77d85d2f604558cacfe50991373e7776b8ed9d3
ls <itiapp@163.com>
2017-02-15 15:31:43 +0800

    提交

--- LSProgressHUDExample/ViewController.m
+++ LSProgressHUDExample/ViewController.m
@@ -16,7 +16,7 @@
 
 
 - (IBAction)show:(id)sender {
-        [LSProgressHUD showWithMessage:@"正在载入"];
+        [LSProgressHUD showWithMessage:nil];
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [LSProgressHUD hide];
         });
